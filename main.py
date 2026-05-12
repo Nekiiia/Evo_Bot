fa06a91f3a35e46432bee9ab19286c7b2f120048
Nekiiia <88255035+Nekiiia@users.noreply.github.com>
2026-05-12 16:51:15 +0300

    Update main.py

--- main.py
+++ main.py
@@ -2,6 +2,7 @@ import asyncio
 import logging
 import os
 
+from aiohttp import web
 from aiogram import Bot, Dispatcher
 from aiogram.client.default import DefaultBotProperties
 from aiogram.enums import ParseMode
@@ -10,8 +11,6 @@ from config import config
 from database.db import init_db, clear_all_carts
 from handlers.user import user_router
 from handlers.order import order_router
-from products import products  
-from aiohttp import web
 
 logging.basicConfig(level=logging.INFO)
 
@@ -19,23 +18,11 @@ logging.basicConfig(level=logging.INFO)
 async def health_check(request):
     return web.Response(text="Bot is running")
 
-async def start_web():
-    app = web.Application()
-    app.router.add_get("/", health_check)
-
-    runner = web.AppRunner(app)
-    await runner.setup()
-
-    port = int(os.environ.get("PORT", 10000))
-
-    print(f"🌐 Web server running on port {port}")
-
-    site = web.TCPSite(runner, "0.0.0.0", port)
-    await site.start()
 
 async def main():
     await init_db()
     await clear_all_carts()
+
     print("🧹 Старые корзины очищены | Бот запущен")
 
     bot = Bot(
@@ -47,19 +34,22 @@ async def main():
     dp.include_router(user_router)
     dp.include_router(order_router)
 
-    # запускаем aiohttp сервер
-    runner = web.AppRunner(web.Application())
+    # --- web server ---
     app = web.Application()
-    app.router.add_get("/", lambda r: web.Response(text="Bot is running"))
+    app.router.add_get("/", health_check)
 
     runner = web.AppRunner(app)
     await runner.setup()
 
     port = int(os.environ.get("PORT", 10000))
     site = web.TCPSite(runner, "0.0.0.0", port)
-    await site.start()
 
+    await site.start()
     print(f"🌐 Web server running on port {port}")
 
-    # запускаем polling (БЛОКИРУЕТ поток — это нормально)
+    # --- bot ---
     await dp.start_polling(bot)
+
+
+if __name__ == "__main__":
+    asyncio.run(main())
