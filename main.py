eec7e4c0d3fad2576358b77864367e761d060d5c
Nekiiia <88255035+Nekiiia@users.noreply.github.com>
2026-05-12 16:18:36 +0300

    up

--- main.py
+++ main.py
@@ -1,5 +1,7 @@
 import asyncio
 import logging
+import os
+
 from aiogram import Bot, Dispatcher
 from aiogram.client.default import DefaultBotProperties
 from aiogram.enums import ParseMode
@@ -9,9 +11,24 @@ from database.db import init_db, clear_all_carts
 from handlers.user import user_router
 from handlers.order import order_router
 from products import products  
+from aiohttp import web
 
 logging.basicConfig(level=logging.INFO)
 
+
+async def health_check(request):
+    return web.Response(text="Bot is running")
+
+async def start_web():
+    app = web.Application()
+    app.router.add_get("/", health_check)
+
+    runner = web.AppRunner(app)
+    await runner.setup()
+
+    port = int(os.environ.get("PORT", 10000))
+    site = web.TCPSite(runner, "0.0.0.0", port)
+    await site.start()
 
 async def main():
     await init_db()
@@ -22,13 +39,19 @@ async def main():
         token=config.BOT_TOKEN,
         default=DefaultBotProperties(parse_mode=ParseMode.HTML)
     )
-    dp = Dispatcher()
 
+    dp = Dispatcher()
     dp.include_router(user_router)
     dp.include_router(order_router)
 
+    # запускаем web сервер
+    asyncio.create_task(start_web())
+
+    # даём web серверу стартануть
+    await asyncio.sleep(1)
+
+    # polling (основной процесс)
     await dp.start_polling(bot)
 
-
-if __name__ == "__main__":
-    asyncio.run(main())
+print(f"🌐 Web server running on port {port}")
+print("🤖 Bot polling started")
